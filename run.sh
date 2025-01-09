fb6156f7890e716f4f3a4ad5d13ae26fa38af5f9
Gradius802 <55552691+Gradius802@users.noreply.github.com>
2025-01-09 14:11:45 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env sh
-
-# Check if Phinx is installed in vendor/bin and run migrations
-if [ -f "./vendor/bin/phinx" ]; then
-    echo "Running Phinx migrations..."
-    ./vendor/bin/phinx migrate
-    if [ $? -ne 0 ]; then
-        echo "Phinx migrations failed!" >&2
-        exit 1
-    fi
-else
-    echo "Phinx not found. Please ensure it's installed by running 'composer install'." >&2
-    exit 1
-fi
-
-# Start PHP built-in server
-echo "Starting PHP"
+./vendor/bin/phinx migrate && \
 php -S 0.0.0.0:8080 -t public
